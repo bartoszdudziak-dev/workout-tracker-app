86323f65c13090cf78bace2043d95c6040a161de
“Bartosz <bartoszdudziak.dev@gmail.com>
2024-10-23 20:32:05 +0200

    Logged as, not found searching features

--- src/features/dashboard/ExerciseProgress.jsx
+++ src/features/dashboard/ExerciseProgress.jsx
@@ -16,6 +16,9 @@ function ExerciseProgress() {
     <div
       className={`mx-auto w-full max-w-screen-xs space-y-8 rounded-md bg-primary px-4 py-6 shadow-md md:px-8 md:py-10 xl:max-w-full ${!isNavOpen && 'lg:max-w-full'} `}
     >
+      <h2 className='text-xl font-bold uppercase tracking-widest text-accent-primary sm:text-2xl'>
+        Check progress
+      </h2>
       <div
         className={`flex flex-col gap-4 lg:gap-8 xl:flex-row xl:items-end ${!isNavOpen && 'lg:flex-row lg:items-end'}`}
       >
@@ -25,14 +28,22 @@ function ExerciseProgress() {
             <Input id='searchExercisesInput' onChange={handleChange} />
           </FormRow>
         </div>
-        {!isSearching && data && data.length > 0 && (
+        {!isSearching && data && data?.length > 0 && (
           <span className='md:text-x text-center text-lg font-bold uppercase tracking-wider text-accent-primary'>
             {data.at(0).exercise_name}
           </span>
         )}
       </div>
 
-      {isSearching ? <Spinner /> : <ProgressChart data={data} />}
+      {isSearching ? (
+        <Spinner />
+      ) : data && data.length === 0 ? (
+        <p className='text-center text-xs font-semibold uppercase tracking-wide text-secondary'>
+          Exercise not found
+        </p>
+      ) : (
+        <ProgressChart data={data} />
+      )}
     </div>
   );
 }
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,11 +1,20 @@
 import Logo from './Logo';
 import PrimaryNav from './PrimaryNav';
+import { useUser } from '../features/authentication/useUser';
 
 function Sidebar() {
+  const { user } = useUser();
+
   return (
-    <aside className='row-span-full space-y-28 p-6'>
+    <aside className='row-span-full grid p-6'>
       <Logo />
       <PrimaryNav />
+      <span className='self-end text-sm tracking-wider'>
+        Logged as{' '}
+        <span className='font-semibold uppercase text-accent-primary'>
+          {user.user_metadata.firstName}
+        </span>
+      </span>
     </aside>
   );
 }
